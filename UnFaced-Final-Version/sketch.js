f1cfe8a4b803283a57330d8ca2d9c807f00e32d1
SuQichen777 <hdredbean123123123@outlook.com>
2025-12-05 02:11:22 +0800

    Add "Face" as beginning default page

--- UnFaced-Final-Version/sketch.js
+++ UnFaced-Final-Version/sketch.js
@@ -1,5 +1,9 @@
 import { state } from "./state.js";
-import { sceneBounds, scrollingMaxSpd } from "./constants.js";
+import {
+  sceneBounds,
+  scrollingMaxSpd,
+  defaultPageOptions,
+} from "./constants.js";
 import {
   createLayers,
   handLayerInitialize,
@@ -12,8 +16,50 @@ import {
 } from "./layers.js";
 import { Scene, scene1, scene2, scene3, scene4 } from "./scenes.js";
 
+// export const state = {
+//   storyStarted: false,
+//   currentScrollingPosition: 0,
+//   scenes: [],
+// };
+
+// Default Page Data
+let faceMesh,
+  video,
+  randomnessControl = 0.7,
+  faces = [],
+  frozenFaces = [],
+  isFaceFrozen = false,
+  options = defaultPageOptions,
+  frozenCircleRadius = 2,
+  randomChar = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "&",
+    "^",
+    "@",
+    "!",
+  ];
+
+function preload() {
+  faceMesh = ml5.faceMesh(options);
+}
+
 function setup() {
-  let canvas = createCanvas(500, 300);
+  let canvas = createCanvas(1000, 600);
+  frozenCircleRadius = (2 / 500) * width;
+  // detect face
+  video = createCapture(VIDEO);
+  video.size(height, width);
+  video.hide();
+  faceMesh.detectStart(video, gotFaces);
   createLayers(width, height);
   state.currentScrollingPosition = 0;
   state.scenes = [
@@ -33,25 +79,63 @@ function setup() {
 }
 
 function draw() {
-  background(0);
-  image(envLayer, 0, 0);
-  handLayer.clear();
-  leftHand.update(0);
-  leftHand.display(handLayer);
-  image(handLayer, 0, height - handLayer.height);
-  if (
-    state.currentScrollingPosition >
-    state.scenes[state.scenes.length - 1].end
-  ) {
-    state.currentScrollingPosition =
-      state.scenes[state.scenes.length - 1].end;
+  if (state.storyStarted) {
+    background(0);
+    image(envLayer, 0, 0);
+    handLayer.clear();
+    leftHand.update(0);
+    leftHand.display(handLayer);
+    image(handLayer, 0, height - handLayer.height);
+    if (
+      state.currentScrollingPosition > state.scenes[state.scenes.length - 1].end
+    ) {
+      state.currentScrollingPosition =
+        state.scenes[state.scenes.length - 1].end;
+    }
+    let activeScene = state.scenes.find((scene) =>
+      scene.contains(state.currentScrollingPosition)
+    );
+    if (activeScene) {
+      activeScene.render();
+    }
+  } else {
+    // click to start
+    defaultPage();
   }
-  let activeScene = state.scenes.find((scene) =>
-    scene.contains(state.currentScrollingPosition)
-  );
-  if (activeScene) {
-    activeScene.render();
+}
+
+function defaultPage() {
+  push();
+  translate(width, 0);
+  scale(-1, 1);
+  background(255);
+  let targetFaces = isFaceFrozen ? frozenFaces : faces;
+  // moving the data points
+  if (isFaceFrozen) {
+    noStroke();
+    fill(0);
+    for (let i = 0; i < targetFaces.length; i++) {
+      const face = targetFaces[i];
+      for (let j = 0; j < face.keypoints.length; j++) {
+        const keypoint = face.keypoints[j];
+        circle(keypoint.x, keypoint.y, frozenCircleRadius * 2);
+      }
+    }
+  } else {
+    textSize(15);
+    fill(0);
+    for (let i = 0; i < targetFaces.length; i++) {
+      let face = targetFaces[i];
+      for (let j = 0; j < face.keypoints.length; j++) {
+        let keypoint = face.keypoints[j];
+        noStroke();
+        if (keypoint.ch) {
+          text(keypoint.ch, keypoint.x, keypoint.y);
+        }
+      }
+    }
   }
+  pop();
 }
 
 function mouseWheel(event) {
@@ -65,6 +149,34 @@ function mouseWheel(event) {
   }
 }
 
+function mousePressed() {
+  if (!state.storyStarted) {
+    isFaceFrozen = true;
+    frozenFaces = faces.map((face) => {
+      return {
+        ...face,
+        keypoints: face.keypoints.map((kp) => ({ ...kp })),
+      };
+    });
+    // state.storyStarted = true;
+  }
+}
+
+function gotFaces(results) {
+  // set cache if not detected
+  if (!isFaceFrozen && results && results.length > 0) {
+    faces = results.map((face) => ({
+      ...face,
+      keypoints: face.keypoints.map((kp) => ({
+        ...kp,
+        ch: random(0, 1) < randomnessControl ? random(randomChar) : null,
+      })),
+    }));
+  }
+}
+
+window.preload = preload;
 window.setup = setup;
 window.draw = draw;
 window.mouseWheel = mouseWheel;
+window.mousePressed = mousePressed;
